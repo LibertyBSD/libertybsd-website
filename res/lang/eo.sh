91748a7c1a155760a3ab5a540be833bbb6982157
Jaidyn Lev <jadedctrl@teknik.io>
2019-01-07 18:18:35 -0600

    Added Esperanto traduko

---
+++ res/lang/eo.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# INDEKSO
+##################
+
+index_raddle="/f/libertybsd ĉe <a href='https://raddle.me'>raddle</a> &#x1f44d;"
+index_reddit="/r/libertybsd ĉe <a href='https://reddit.com'>reddit</a> &#x1f44e;"
+index_freenode="#libertybsd ĉe <a href='https://freenode.net'>Freenode</a> &#x2605;"
+
+
+index_p1="Universale, <a href='https://openbsd.org'>OpenBSD</a> estas konata
+pro sia sekureco– fiere, la slogano estas: &ldquo;Nur du foraj truoj en la norma
+instalaĵo, dum ege longa tempo!&rdquo;"
+
+index_p2="Sed, OpenBSD enhavas multe da
+<a href='https://cvsweb.openbsd.org/cgi-bin/cvsweb/src/sys/dev/microcode'>
+malliberaj, nurduumaj, mikroprogrmamoj</a> en la baza sistemo. Ĝi ankaŭ elŝutas
+pliajn mikroprogramojn, depende de via komputilo."
+
+index_p3="Kun la norma instalaĵo, vi eble uzus iom da la mallibreraj programoj–
+senscie! Kaj se vi provus instali pliajn programojn, eble vi hazarde instalus
+kelkajn malliberajn."
+
+index_p4="LibertyBSD estas “malbloba” varianto de OpenBSD. Vi povas havi ĉiujn
+de la avantaĝoj de OpenBSD kaj certi, ke vi ne havas malliberajn programojn en
+via operaciumo."
+
+
+
+
+# ELŜUTOJ
+##################
+
+download_downloads="Elŝutoj"
+download_host="Gastiganto"
+download_type="Tipo"
+download_location="Loko"
+download_protocols="Protokoloj"
+download_current_mirrors="Nunaj Speguloj"
+download_old_mirrors="Praaj Speguloj"
+download_last_version="Lastaj Versioj"
+download_full="Plena"
+download_install_only="Nurinstala"
+
+
+location_chiba_japan="Ĉiba, Japano"
+location_usa="Usono"
+location_texas_usa="Teksaso, Usono"
